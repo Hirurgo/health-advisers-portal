3475e4022272199b3e3e7df07f7892dd314dcbd6
Mikalai <keep_ya_head_up@inbox.ru>
2021-09-28 20:40:52 +0300

    switch app ID

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <div className="App">
       <SendBirdApp
-        appId={'404E369B-11BC-40B0-A950-DBBAA9896B59'}
-        userId={'HelthAdviser1'}
+        appId={'0C34BECA-CD93-4409-8CC6-AA6C5DB3B819'}
+        userId={'HealthAdviser1'}
+        accessToken={'a178dab1c39df763b5af7ced8d89741a1e78349d'}
         showSearchIcon
       />
     </div>
